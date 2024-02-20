e252e6b59282428bcb22a451622c45ecfa837812
kathy nkelenda <kathynkelenda1@gmail.com>
2024-02-20 20:11:19 +0100

    send mail wich contains a link to reset password(function name:forgotPassword)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,7 @@ const connection = require('../connection');
 const router = express.Router(); //Création du router
 const jsonwebtoken = require('jsonwebtoken');
 require('dotenv').config();
+const nodemailer = require('nodemailer');
 
 //Inscription
 router.post('/signUp', (req, res) => {
@@ -58,5 +59,51 @@ router.post('/login', (req, res) => {
 })
 
 
-module.exports = router;
 
+//Forget password : Create transporter
+var transporter = nodemailer.createTransport({
+    service : 'gmail',
+    auth:{
+        user:process.env.EMAIL,
+        pass:process.env.PASSWORD    
+   }
+});
+
+
+router.post('/forgotPassword',(req,res)=>{
+    const user = req.body;
+    query = "select email,password from user_ where email=?"
+
+    connection.query(query,[user.email],(error,results)=>{
+        if(!error){
+            /*L'email n'existe pas ds la bdd et pr ne pas pouvoir se rendre compte de l'exterieur
+             qu'il en est ainsi, on écrit le message à l'utilisateur.*/
+            if(results.length <=0){
+                return res.status(200).json({message:"If you've this email adress, you'll receive a password"})
+            }else{
+                var mailOptions = {
+                    from:process.env.EMAIL,
+                    to:results[0].email,
+                    subject:'Password send by R_Management',
+                    html:'<p> <b>Your Login details for R_Management System</b><br><b>Email:</b>'+results[0].email
+                    +'<br><b>Password: </b>'+results[0].password
+                    +'<a href=" http://localhost:4200/ ">Click here to login</a> </p>'
+                };
+
+                transporter.sendMail(mailOptions,function(error,info){
+                    if(error){
+                        console.log(error);
+                    }else{
+                        console.log('Email sent'+ info.response);
+                    }
+                });
+                return res.status(200).json({message:"Password send!"})
+            }
+        }else{
+            return res.status(500).json({error})
+        }
+    })
+})
+
+
+module.exports = router;
